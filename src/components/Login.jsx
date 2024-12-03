da15c833bd663d62211ee2557ebb7d3ba6811e36
Gursewak Singh <gursewaksinghynr73@gmail.com>
2024-12-03 13:57:57 +0530

    error resolve

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
   const handleLogin = async () => {
     try {
       const response = await axios.post(
-        "http://localhost:5001/api/login",
+        "https://entnt-backend-6c8l.onrender.com/api/login",
         formData
       );
       console.log(response)
